d125fe5ff042d8bc60968c55406a03d8119df5a1
Alebatistasilva <silva.alebatista@yahoo.com.br>
2025-01-30 23:36:53 -0300

    novo commit

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,8 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("**********");
+        System.out.println("*************");
         System.out.println("Olá mundo!");
-        System.out.println("**********");
+        System.out.println("Esta tudo bem");
+        System.out.println("**************");
 
     }
 }
